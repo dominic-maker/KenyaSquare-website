efae38a9aa40fb3d4ac3ebb5c0045d98e59ffdae
dominic-maker <dominicmuturi2018@gmail.com>
2023-03-31 14:00:35 +0300

    first commit

---
+++ grocery/forms.py
@@ -0,0 +1,7 @@
+﻿from django import forms
+from.models import item
+
+class itemForm(forms.ModelForm):
+    class Meta:
+        model= item
+        fields= ["category","item_img","item_name","price","discount_price","special_offer","stock","available"]
